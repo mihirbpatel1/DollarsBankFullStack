d69985564d5c615000316696f92b92ab3cbe84fe
mihirbpatel1 <mihirbpatel96@gmail.com>
2022-01-10 19:57:13 -0600

    First commit

---
+++ src/main/java/com/spring/jump/DollarsBankFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollarsBankFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollarsBankFullStackApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/spring/jump/model/Tranfer.java
@@ -0,0 +1,5 @@
+package com.spring.jump.model;
+
+public class Tranfer {
+
+}
